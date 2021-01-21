b2d157722bdf6520d3a3b7fa2fda812b63d39f01
Forroukh <forroukhahmedr@gmail.com>
2021-01-22 00:22:27 +0600

    kilometerToMeter

---
+++ assignment.js
@@ -0,0 +1,8 @@
+//Function  Kilometer to Meter
+var meter = 1000;
+function kilometerToMeter(meter) {
+    var  kilometer = meter*1000;
+    return kilometer;
+}
+var meterCalculation = kilometerToMeter(1);
+console.log(meterCalculation);
